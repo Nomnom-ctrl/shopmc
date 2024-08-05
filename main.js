1c1a46b8b3e80100c555e2696fc7e951aeafdb07
Nomnom-ctrl <66309010007@thonburi.ac.th>
2024-08-05 14:51:15 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector('#menu-icon');
+let navber = document.querySelector('.navber');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navber.classList.toggle('active')
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navber.classList.remove('active')
+}
